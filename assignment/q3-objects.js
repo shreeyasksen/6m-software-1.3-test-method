0ed467cf04661a180360eaff9af3216930449e96
shreeyasksen <104299487+shreeyasksen@users.noreply.github.com>
2023-08-27 00:42:35 +0800

    Update q3-objects.js answer

--- assignment/q3-objects.js
+++ assignment/q3-objects.js
@@ -25,6 +25,21 @@ function convert(keyArr, valueArr){
         - Step 3: Within the for-loop, add the key and value to the local scoped object.
         - Step 4: Write a return statement to return the object literal after the for-loop code block.
     */
+    let obj = {}
+     for (let i = 0; i < keyArr.length; i++) {
+        obj[keyArr[i]] = valueArr[i];
+    }
+    
+    return obj;
+}
+const booksobj = convert(bookIdArr, bookTitle);
+
+console.log(booksobj);
+function printByKey(key){
+    console.log(booksObj[key]);
+}
+
+booksObj = convert(bookIdArr, bookTitle);
 }
 
 function printByKey(key){
