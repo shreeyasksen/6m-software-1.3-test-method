252fe5199e5719299e7b9571a587f94845c50e93
shreeyasksen <104299487+shreeyasksen@users.noreply.github.com>
2023-08-27 00:40:31 +0800

    Update q2-arrays-loop.js answer

--- assignment/q2-arrays-loop.js
+++ assignment/q2-arrays-loop.js
@@ -8,10 +8,10 @@ const studentList = ["Johnson", "Christina", "Michael", "Lina", "Larry", "Michel
 
 function printStudentNames(){
     // Add code here
+    for(let i = 0;i<studentList.length;i++)
+   console.log(studentList[i]);
 }
-
 printStudentNames();
-
 // Ignore the code below this line
 module.exports = {
     studentList,
